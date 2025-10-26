d3bb9c1a6ccb188481f66956a2398ad28a9049a3
Jakha Cham <you@example.com>
2025-10-25 23:54:32 -0400

    merge sort done

--- A6-template/MergeSort.java
+++ A6-template/MergeSort.java
@@ -1,9 +1,11 @@
 import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class MergeSort {
   
   public static CardPile sort(CardPile unsorted, SortRecorder record) {
-    
+    record.add(unsorted);
     ArrayDeque<CardPile> queue = new ArrayDeque<CardPile>();
   
     // ***********************************************************
@@ -16,8 +18,73 @@ public class MergeSort {
     //           record.add(pile);
     //        }
     // ***********************************************************
+    Iterator<Card> iterator = unsorted.iterator();
 
+    //make each card its own pile and add to queue
+    while(iterator.hasNext()){
+      Card nextCard = iterator.next();
+      CardPile singleCardPile = new CardPile();
+      singleCardPile.add(nextCard);
+      queue.addLast(singleCardPile);
+    }
+
+    //merge piles until only one pile remains
+    while(queue.size() > 1){
+      //remove first two piles from queue
+      CardPile pile1 = queue.remove();
+      CardPile pile2 = queue.remove();
+
+      //merge the two piles
+      CardPile mergedPile = merge(pile1, pile2);
+
+      //add merged pile back to queue
+      queue.addLast(mergedPile);
+      //record progress
+      record.next();
+      record.add(mergedPile);
+    }
     // return the sorted result here
-    return queue.remove();
+    CardPile sortedPile = queue.removeFirst();
+    return sortedPile;
+  }
+    //return queue.remove();
+
+  public static CardPile merge(CardPile leftPile, CardPile rightPile) {
+    CardPile mergedPile = new CardPile();
+
+    while(!leftPile.isEmpty() && !rightPile.isEmpty()) {
+      if (leftPile.peek().compareTo(rightPile.peek()) <= 0) {
+        mergedPile.add(leftPile.removeFirst());
+      } 
+      else {
+        mergedPile.add(rightPile.removeFirst());
+      }
+    }
+    //add remaining cards from left pile, if any
+    while(!leftPile.isEmpty()) {
+      mergedPile.add(leftPile.removeFirst());
+    }
+    //add remaining cards from right pile, if any
+    while(!rightPile.isEmpty()) {
+      mergedPile.add(rightPile.removeFirst());  
+    }
+    return mergedPile;
+  }
+
+  public static void main(String args[]) {  
+      //set up sorting recorder
+      SortRecorder recorder = new SortRecorder();
+
+      //load card images
+      Card.loadImages(recorder);
+
+      //make deck
+      CardPile deck = new CardPile(Card.newDeck(true), 2, 2);
+
+      Collections.shuffle(deck);
+      deck = MergeSort.sort(deck, recorder);
+
+      recorder.display("Card Sort Demo: MergeSort");
+      
   }
 }
