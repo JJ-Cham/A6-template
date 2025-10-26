c9cd51452d9176c09a425a3627dbf8e271b62f7b
Jakha Cham <you@example.com>
2025-10-25 22:45:06 -0400

    inserttion sort doen

--- A6-template/InsertionSort.java
+++ A6-template/InsertionSort.java
@@ -1,3 +1,6 @@
+import java.util.Collections;
+import java.util.Iterator;
+
 public class InsertionSort {
   
   public static CardPile sort(CardPile unsorted, SortRecorder record) {
@@ -15,8 +18,48 @@ public class InsertionSort {
     //     recorder after each card is transferred:
 
     // ***********************************************************
+    while(unsorted.size()>0){
 
+      //taking a card from unsorted pile
+      Card nextCard = unsorted.removeFirst();
+
+      //find where it should go in sorted pile
+      int insertIndex = 0;
+      Iterator<Card> iterator = sorted.iterator();
+      while(iterator.hasNext()){
+        Card currentCard = iterator.next();
+        if(nextCard.compareTo(currentCard) < 0){
+          //stop when we find a card larger than nextCard
+          break;
+      }
+        insertIndex++; //move to next index
+      }
+      //insert the card into sorted pile at the correct index
+      sorted.add(insertIndex, nextCard);
+
+      // record progress
+      record.next();
+      record.add(sorted);
+      record.add(unsorted);
+    }
     // return the sorted result here
     return sorted;
+  }
+
+  public static void main(String args[]) {  
+      //set up sorting recorder
+      SortRecorder recorder = new SortRecorder();
+
+      //load card images
+      Card.loadImages(recorder);
+
+      //make deck
+      CardPile cards = new CardPile(Card.newDeck(true), 2, 2);
+
+      Collections.shuffle(cards);
+      cards = InsertionSort.sort(cards, recorder);
+
+     recorder.display("Card Sort Demo: InsertionSort");
+      
   }
 }
