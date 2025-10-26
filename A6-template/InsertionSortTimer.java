35be15e7833dc7106c1357c04f5aef771e3e47df
Jakha Cham <you@example.com>
2025-10-26 00:27:03 -0400

    timers

--- A6-template/InsertionSortTimer.java
+++ A6-template/InsertionSortTimer.java
@@ -1,3 +1,66 @@
+import java.util.Collections;
+import java.util.Iterator;
+
 public class InsertionSortTimer {
+  
+  public static CardPile sort(CardPile unsorted) {
     
+    // register the starting configuration with the recorder
+    //record.add(unsorted);
+
+    // Here is the result list you will be creating
+    CardPile sorted = new CardPile();
+  
+    // ***********************************************************
+    // Here is where you'll do the "work" of InsertionSort:
+    //   - Use sorted to store the "sorted portion"
+    //   - Don't forget to register the new state with the
+    //     recorder after each card is transferred:
+
+    // ***********************************************************
+    while(unsorted.size()>0){
+
+      //taking a card from unsorted pile
+      Card nextCard = unsorted.removeFirst();
+
+      //find where it should go in sorted pile
+      int insertIndex = 0;
+      Iterator<Card> iterator = sorted.iterator();
+      while(iterator.hasNext()){
+        Card currentCard = iterator.next();
+        if(nextCard.compareTo(currentCard) < 0){
+          //stop when we find a card larger than nextCard
+          break;
+      }
+        insertIndex++; //move to next index
+      }
+      //insert the card into sorted pile at the correct index
+      sorted.add(insertIndex, nextCard);
+
+      // record progress
+      //record.next();
+      //record.add(sorted);
+      //record.add(unsorted);
+    }
+    // return the sorted result here
+    return sorted;
+  }
+
+  public static void main(String args[]) {  
+      //set up sorting recorder
+      SortRecorder recorder = new SortRecorder();
+
+      //load card images
+      Card.loadImages(recorder);
+
+      //make deck
+      CardPile cards = new CardPile(Card.newDeck(true), 2, 2);
+
+      Collections.shuffle(cards);
+      cards = InsertionSort.sort(cards, recorder);
+
+     recorder.display("Card Sort Demo: InsertionSort");
+      
+  }
 }
+
--- A6-template/SelectionSortTimer.java
+++ A6-template/SelectionSortTimer.java
@@ -1,3 +1,80 @@
+import java.util.Collections;
+import java.util.Iterator;
+
 public class SelectionSortTimer {
+  
+  public static CardPile sort(CardPile unsorted) {
     
+    // register the starting configuration with the recorder
+    //record.add(unsorted);
+
+    // Here is the result list you will be creating
+    CardPile sorted = new CardPile();
+    //CardPile unsorted = new CardPile(); 
+  
+    // ***********************************************************
+    // Here is where you'll do the "work" of SelectionSort:
+    //   - Use sorted to store the "sorted portion"
+    //   - Don't forget to register the new state with the
+    //     recorder after each card is transferred:
+    //        record.next();        // tell it this is a new step
+    //        record.add(sorted);   // the sorted pile
+    //        record.add(unsorted); // the unsorted pile
+    // ***********************************************************
+    Collections.shuffle(unsorted); //shuffle unsorted pile to start
+    // //record progress
+    while (unsorted.size() > 0) {
+      // move one card between piles
+      //find smallest card in unsorted pile
+      Card smallestCard = findSmallest(unsorted);
+      //remove it 
+      unsorted.remove(smallestCard);
+      //add it to sorted pile
+      sorted.addLast(smallestCard);
+      // record progress
+      //record.next();
+      //record.add(sorted);
+      //record.add(unsorted);
+    }
+    return sorted;
+  }
+
+    //iterate through unsorted pile to find smallest card
+  public static Card findSmallest(CardPile pile) {
+      Iterator<Card> iterator = pile.iterator();
+      Card smallestCard = iterator.next();
+      while (iterator.hasNext()) {
+        Card nextCard = iterator.next();
+        if (nextCard.compareTo(smallestCard) < 0) {
+          smallestCard = nextCard;
+        }
+      }
+      return smallestCard;
+    }
+
+  public static void main(String args[]) {  
+      //set up sorting recorder
+      SortRecorder recorder = new SortRecorder();
+
+      //load card images
+      Card.loadImages(recorder);
+
+      //make new deck 
+      CardPile deck = new CardPile(Card.newDeck(true), 2, 2);
+
+      //shuffle
+      Collections.shuffle(deck);
+
+      //sort the deck
+      deck = SelectionSort.sort(deck, recorder);
+
+      //print sorted deck
+      System.out.println(deck);
+
+      //Display the animation
+      recorder.display("Selection Sort Animation");
+
+    // return the sorted result here
+    //return sorted;
+  }
 }
