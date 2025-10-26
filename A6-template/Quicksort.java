db9fc0e9ab1abb73431cc53eef69c5d5d57fcdcd
Jakha Cham <you@example.com>
2025-10-26 16:53:34 -0400

    phase 1 done, quicksort done

--- A6-template/Quicksort.java
+++ A6-template/Quicksort.java
@@ -1,12 +1,16 @@
+import java.util.Collections;
+
 public class Quicksort {
   
   public static CardPile sort(CardPile unsorted, SortRecorder record) {
-
+    //reursion
     // ***********************************************************
     // Here is where you'll check the stop condition and return
     // if it is satisfied.
     // ***********************************************************
-    
+    if(unsorted.size()<=1){
+      return unsorted; //base case, basically already sorted
+    }
     // Here are the two partitions you will be creating
     CardPile smaller = new CardPile();
     CardPile bigger = new CardPile();
@@ -16,7 +20,7 @@ public class Quicksort {
     //   - Choose a pivot
     //   - Partition the unsorted list into two piles
     // ***********************************************************
-    Card pivot = null;  // edit this!
+    Card pivot = unsorted.removeFirst();  // edit this!
     
     // register the partitions with the recorder
     record.add(smaller);
@@ -32,6 +36,27 @@ public class Quicksort {
     //   - Make recursive calls on the partitions
     //   - Assemble the sorted results into a single pile
     // ***********************************************************
+    while(unsorted.size()>0){
+      Card nextCard = unsorted.removeFirst();
+      if(nextCard.compareTo(pivot) < 0){
+        smaller.addLast(nextCard);
+      }
+      else{
+        bigger.addLast(nextCard);
+      }
+    }
+
+    //recursive calls
+    CardPile sortedSmaller = sort(smaller, record);
+    CardPile sortedBigger = sort(bigger, record);
+
+    //result 
+    CardPile resultPile = new CardPile();
+
+    //add all the sorted piles
+    resultPile.addAll(sortedSmaller);
+    resultPile.addLast(pivot);
+    resultPile.addAll(sortedBigger);
 
     // record the sorted result
     record.add(result);
@@ -39,5 +64,22 @@ public class Quicksort {
     
     // return the sorted result here
     return result;
+  }
+
+  public static void main(String args[]) {  
+      //set up sorting recorder
+      SortRecorder recorder = new SortRecorder();
+
+      //load card images
+      Card.loadImages(recorder);
+
+      //make deck
+      CardPile deck = new CardPile(Card.newDeck(true), 2, 2);
+
+      Collections.shuffle(deck);
+      deck = Quicksort.sort(deck, recorder);
+
+      //show the sorting process
+      recorder.display("Card Sort Demo: Quicksort");
   }
 }
